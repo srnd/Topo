27e2737eeca76eb6b0544dac78fc437c5ebef844
Tyler Menezes <tylermenezes@gmail.com>
2019-09-20 17:01:31 -0400

    Update order of imports

--- src/Logo/index.js
+++ src/Logo/index.js
@@ -1,8 +1,8 @@
+import CodeDayHeart from './CodeDayHeart'
+import Srnd from './Srnd'
 export { default as CodeDayHeart } from './CodeDayHeart'
 export { default as Srnd } from './Srnd'
 
-import CodeDayHeart from './CodeDayHeart'
-import Srnd from './Srnd'
 export default {
   CodeDayHeart,
   Srnd,
